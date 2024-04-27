8f1192a617154171de1a38e02dfcc5d88902621c
ogmj <ogmj98@gmail.com>
2024-04-27 14:52:27 +0900

    [정희철] 사전과제 시작

---
+++ PreAssignment/PreAssignment.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <cassert>
+
+#include <random>
+#include <map>
+using namespace std;
+
+void spilt(vector<string>& command, string input) {
+    string elem;
+    elem.clear();
+    for (auto& i : input) {
+        if (i == ' ') {
+            command.push_back(elem);
+            elem.clear();
+        }
+        else {
+            elem += i;
+        }
+    }
+    command.push_back(elem);
+}
+
+
+
+int main()
+{
+    char input[512];
+    vector<string> command;
+
+    while (1) {
+        command.clear();
+        cin.getline(input, sizeof(input));
+        spilt(command, input);
+        if (!command.empty()) {
+            if (command[0] == "/quit") {
+                break;
+            }
+        }
+    }
+
+    std::cout << "Hello World!\n";
+}
+
