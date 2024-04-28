b663c7542dd606f0be5910756010471804f2af62
ogmj <ogmj98@gmail.com>
2024-04-28 13:14:23 +0900

    [정희철] 중복 사용이 예측되는 코드를 함수로 변경

--- PreAssignment/Random.h
+++ PreAssignment/Random.h
@@ -7,28 +7,28 @@
 
 using namespace std;
 
-template <typename T>
+template <typename T1>
 class random {
 public:
-	random(){};
+	random()
+	{
+		m_vecData.reserve(16);
+	};
 	~random() = default;
 
-	void AddRandom(int Preb, const T& Data) {
+	void AddRandom(int Preb, const T1& Data) {
 		if (m_vecData.empty()) {
 			m_vecData.push_back({ Preb, Data });
 		}
 		else {
-			m_vecData.push_back({ m_vecData.back().first + Preb, Data });
+			int key = m_vecData.back().first;
+			m_vecData.push_back({ key + Preb, Data });
 		}
 	}
 
-	bool GetRandom(T& Data) const {
-
-		std::mt19937_64 gen(std::chrono::system_clock::now().time_since_epoch().count());
-		std::uniform_int_distribution<int> r(1, m_vecData.back().first);
-		int key = r(gen);
-		cout << "random value :" << key << "\n";
-		auto elem = lower_bound(m_vecData.begin(), m_vecData.end(), key, [](pair<int, const T&> a, int b) {
+	bool GetRandom(T1& Data) const {
+		auto key = GetRand<int>(1, m_vecData.back().first);
+		auto elem = lower_bound(m_vecData.begin(), m_vecData.end(), key, [](pair<int, const T1&> a, int b) {
 			return a.first < b;
 			});
 		if (elem != m_vecData.end()) {
@@ -36,6 +36,15 @@ public:
 			return true;
 		}
 		return false;
+	}
+
+	template <typename T2>
+	T2 GetRand(T2 begin, T2 end) const {
+		std::mt19937_64 gen(std::chrono::system_clock::now().time_since_epoch().count());
+		std::uniform_int_distribution<T2> r(begin, end);
+		T2 key = r(gen);
+		cout << "random value :" << key << "\n";
+		return key;
 	}
 
 	void Enum() {
@@ -46,5 +55,5 @@ public:
 	}
 
 private:
-	vector<pair<int, const T&>> m_vecData;
+	vector<pair<int, const T1&>> m_vecData;
 };
