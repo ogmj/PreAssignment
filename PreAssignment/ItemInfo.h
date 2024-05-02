b0aec6fb1a9943cfb693a874496a44eb5eff522d
ogmj <ogmj98@gmail.com>
2024-05-02 12:19:25 +0900

    [정희철] 문제2번 iteminfo에 메모리풀 적용

--- PreAssignment/ItemInfo.h
+++ PreAssignment/ItemInfo.h
@@ -1,9 +1,10 @@
 #pragma once
 #include <string>
+#include "../PreAssignment/Util/CMemoryPool.h"
 
 using namespace std;
 
-class ItemInfo {
+class ItemInfo : public MemoryPool<ItemInfo> {
 public:
 	ItemInfo() : mID(0), mName("item"), mClassType(0), mType(0) {};
 	ItemInfo(__int64 id, const char* name, char ct, char t) : mID(id), mName(name), mClassType(ct), mType(t) {};
