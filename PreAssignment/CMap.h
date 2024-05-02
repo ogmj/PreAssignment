b8fd1b97685e94f72425e02ab0eae7372ea1c6d4
ogmj <ogmj98@gmail.com>
2024-05-02 12:19:47 +0900

    [정희철] 문제3번 CMap 클래스 작성

--- PreAssignment/CMap.h
+++ PreAssignment/CMap.h
@@ -3,8 +3,36 @@
 
 class CMap: public MemoryPool<CMap> {
 public:
-	CMap() {};
+	CMap() = delete;
+	explicit CMap( int id, char type, int max ): mIndex(id), mType(type), mMaxPlayerCnt(max), mPlayerCnt(0) {};
 	virtual ~CMap() = default;
 
+	CMap* MakeMap(int id, char type, int max) {
+		return new CMap(id, type, max);
+	}
+
+	void AddPlayer() {
+		++mPlayerCnt;
+	}
+	void RemovePlayer(){
+		--mPlayerCnt;
+	}
+	int GetMaxPlayerCnt() {
+		return mMaxPlayerCnt;
+	}
+	int GetPlayerCnt() {
+		return mPlayerCnt;
+	}
+	char GetType() {
+		return mType;
+	}
+	int GetIndex() {
+		return mIndex;
+	}
+
 private:
+	int mIndex;			//유니크 아이디
+	int mPlayerCnt;
+	int mMaxPlayerCnt;
+	char mType;			//0:기본 1;추가
 };
